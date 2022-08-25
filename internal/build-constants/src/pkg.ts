32ee34d39e2f13628b66a12268ea2961876e41cd
Giraffee <xzc2116@gmail.com>
2022-08-25 08:41:11 +0800

    feat: 漏改的配置

--- internal/build-constants/src/pkg.ts
+++ internal/build-constants/src/pkg.ts
@@ -1,4 +1,4 @@
-export const PKG_PREFIX = '@lt13'
+export const PKG_PREFIX = '@lt13.10'
 export const PKG_NAME = '@lt13.10/element-plus'
 export const PKG_CAMELCASE_NAME = 'ElementPlus'
 export const PKG_CAMELCASE_LOCAL_NAME = 'ElementPlusLocale'
